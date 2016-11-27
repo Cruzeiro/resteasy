19e11e69cacdd4264c5a7f2874dbcbf7e647a979
Stepan Krimskiy <cruze132@gmail.com>
2016-11-27 15:46:08 +0700

    Initial commit

---
+++ src/main/java/com/cruze/server/JsonService.java
@@ -0,0 +1,37 @@
+package com.cruze.server;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/json/product")
+public class JsonService {
+
+    @GET
+    @Path("/get")
+    @Produces("application/json")
+    public Book getProductInJSON() {
+
+        Book book = new Book();
+        book.setId(999);
+        book.setAuthor("Hello");
+        book.setName("World");
+
+        return book;
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes("application/json")
+    public Response createProductInJSON(Book book) {
+
+        String result = "Book created : " + book;
+        return Response.status(201).entity(result).build();
+
+    }
+
+}
